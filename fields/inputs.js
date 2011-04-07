f75813d6cfef52a085dc91dad77b4023ff0095cf
CrypticSwarm <crypticswarm+git@gmail.com>
2011-04-07 16:26:16 -0500

    added hidden input and text area

--- fields/inputs.js
+++ fields/inputs.js
@@ -2,6 +2,8 @@ module.exports = function(validators, widgets, fields, ValidatorError) {
   widgets.text = inputType('text')
   widgets.password = inputType('password')
   widgets.checkbox = inputType('checkbox')
+  widgets.hidden = inputType('hidden')
+  widgets.textArea = textArea
 }
 
 function inputType(type) {
@@ -9,5 +11,15 @@ function inputType(type) {
     buf.push('<input type="' + type + '" name="' + name + '"')
     if (val != null) buf.push(' value="'+ val + '"');
     buf.push(' />');
+  }
+}
+
+function textArea(rows, cols) {
+  rows = rows || 10
+  cols = cols || 30
+  return function(name, val, buf, opts) {
+    buf.push('<textarea name="' + name + '" rows="' + rows + '" cols="' + cols + '">')
+    if (val != null) buf.push(' value="'+ val + '"');
+    buf.push('</textarea>')
   }
 }
