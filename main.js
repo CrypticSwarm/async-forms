710fb00f53528597656d1a10dd95910c11a15387
CrypticSwarm <crypticswarm+git@gmail.com>
2011-06-27 23:20:39 -0500

    package.json added

---
+++ main.js
@@ -0,0 +1,7 @@
+var exports = module.exports = require('./jade-form')
+  , form = require('./forms')
+exports.Form = form.Form
+exports.validator = form.validator
+exports.widgets = form.widgets
+exports.fields = form.fields
+exports.ValidatorError = form.ValidatorError
