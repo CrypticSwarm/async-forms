ab05ca0869f3bec248f01e7aed011079467debab
CrypticSwarm <crypticswarm+git@gmail.com>
2012-02-11 12:11:48 -0600

    removes the need for main. sets entry point as forms.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var exports = module.exports = require('./jade-form')
-  , form = require('./forms')
-exports.Form = form.Form
-exports.validator = form.validator
-exports.widgets = form.widgets
-exports.fields = form.fields
-exports.ValidatorError = form.ValidatorError
